de05765b6b6b161750f183211ab19eb9e8b7066c
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2019-10-21 20:40:00 +0000

    hw7 done

--- dj4e/autos/models.py
+++ dj4e/autos/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinLengthValidator
 
 class Make(models.Model):
     name = models.CharField(
-            max_length=200, 
+            max_length=200,
             help_text='Enter a make (e.g. Dodge)',
             validators=[MinLengthValidator(2, "Make must be greater than 1 character")]
     )
@@ -12,7 +12,7 @@ class Make(models.Model):
         """String for representing the Model object."""
         return self.name
 
-class Auto(models.Model) : 
+class Auto(models.Model) :
     nickname = models.CharField(
             max_length=200,
             validators=[MinLengthValidator(2, "Nickname must be greater than 1 character")]
