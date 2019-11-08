968b1c51f6c2758e1e193c4134fe638f39bd8cda
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2019-11-08 19:34:41 +0000

    ads #2 wip

--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -1,9 +1,12 @@
-from ads.models import Ad
+from ads.models import Ad, Comment
 
 from django.views import View
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from ads.forms import CommentForm
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
 class AdListView(OwnerListView):
@@ -13,17 +16,49 @@ class AdListView(OwnerListView):
 class AdDetailView(OwnerDetailView):
     model = Ad
     template_name = "ads/ad_detail.html"
+    def get(self, request, pk) :
+        ad = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=ad).order_by('-updated_at') # flag
+        comment_form = CommentForm()
+        context = { 'ad' : ad, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
 
 class AdCreateView(OwnerCreateView):
     model = Ad
-    fields = ['title', 'price', 'text']
+    fields = ['title', 'price', 'text', 'picture']
     template_name = "ads/ad_form.html"
 
 class AdUpdateView(OwnerUpdateView):
     model = Ad
-    fields = ['title', 'price', 'text']
+    fields = ['title', 'price', 'text', 'picture']
     template_name = "ads/ad_form.html"
 
 class AdDeleteView(OwnerDeleteView):
     model = Ad
     template_name = "ads/ad_delete.html"
+
+def stream_file(request, pk): # flag
+    pic = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = pic.content_type
+    response['Content-Length'] = len(pic.picture)
+    response.write(pic.picture)
+    return response
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        f = get_object_or_404(Ad, id=pk)
+        comment_form = CommentForm(request.POST)
+
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
+        comment.save()
+        return redirect(reverse('ads:ad_detail', args=[pk]))
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "ads/comment_delete.html"
+
+    # https://stackoverflow.com/questions/26290415/deleteview-with-a-dynamic-success-url-dependent-on-id
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse('ads:ad_detail', args=[ad.id])
